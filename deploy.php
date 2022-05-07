bf34447b7f6a1b1f7124fb56e5b912948ffbf7f9
Paolo Centomani <paolo.centomani@gmail.com>
2022-05-07 16:02:01 +0200

    Configure Deployer 7

---
+++ deploy.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+// Config
+
+set('domain', 'jetstream-boilerplate.test');
+set('repository', 'git@github.com:PaoloCentomani/Jetstream-Boilerplate.git');
+
+add('shared_files', []);
+add('shared_dirs', []);
+add('writable_dirs', []);
+
+// Hosts
+
+host('123.123.123.123')
+    ->set('branch', 'main')
+    ->set('deploy_path', '/var/www/{{ domain }}')
+    ->set('remote_user', 'paolo');
+
+// Tasks
+
+after('deploy:failed', 'deploy:unlock');
+
+task('deploy:assets', function () {
+    runLocally('npm run prod');
+    upload('public/css', '{{release_path}}/public');
+    upload('public/img', '{{release_path}}/public');
+    upload('public/js', '{{release_path}}/public');
+    upload('public/mix-manifest.json', '{{release_path}}/public');
+});
+after('deploy:vendors', 'deploy:assets');
